977c0a643ee51b06fe00c2580567ee8dc453def3
Adham Zahran <lordadamson@gmail.com>
2018-01-21 20:03:51 +0200

    created the make_unique_array_ptr and make_unique_ptr

--- rdi_stl_utils.h
+++ rdi_stl_utils.h
@@ -4,12 +4,32 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <memory>
+
+/// Use this macro to create a smart pointer to an array
+#define MAKE_UNIQUE_ARRAY(TYPE, NAME, SIZE) \
+	std::unique_ptr<TYPE[]> NAME = std::make_unique<TYPE[]>((SIZE))
+
+/// Use this macro when you own an array ptr that you did not create
+/// but scence you own it, you're responsible of freeing it.
+/// Note that the array must be created using the 'new' keyword.
+/// malloc may not produce compile error and the program may run normally
+/// but it can (and will) silently leak memory in the background
+#define MAKE_UNIQUE_ARRAY_PTR(TYPE, NAME, ARRAY) \
+	std::unique_ptr<TYPE, void (*)(TYPE*)> NAME = \
+	std::unique_ptr<TYPE, void (*)(TYPE*)>((ARRAY), unique_array_ptr_deleter)
 
 namespace RDI
 {
 
 extern "C++"
 {
+
+template<typename T>
+void unique_array_ptr_deleter(T* object)
+{
+	delete[] object;
+}
 
 template<typename T>
 std::vector<std::basic_string<T>> split(const std::basic_string<T> &input, const T delimiter = ' ')
@@ -78,6 +98,6 @@ bool within_vector(const T& element, const std::vector<T>& v)
 }
 
 
-}
+} // namespace RDI
 
 #endif // RDI_STL_UTILS_H
